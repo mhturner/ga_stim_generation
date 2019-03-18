4bced368565a5cb1c3a3b1f051acb1e1ce96ab8a
Max Turner <maxwellholteturner@gmail.com>
2019-03-18 11:00:24 -0700

    Split model from expt stimvolver

--- stimvolver/stimulus_evolution_modelRF.py
+++ stimvolver/stimulus_evolution_modelRF.py
@@ -18,11 +18,14 @@ from datetime import datetime
 
 from fiver import imaging_data
 
+import skimage.transform as skt
+from skimage.filters import gabor_kernel
+
 class StimulusEvolver():
     def __init__(self, stim_size = (8,10,20), pop_size = 40, num_persistent_parents = 10,
-                 mutation_rate = 0.02,
+                 mutation_rate = 0.02, noise_level = 0.0, threshold = None,
                  sparsity_penalty = 0.0, similarity_penalty_weight = 0.0, prior_solutions = [],
-                 stimulus_type = 'ternary_dense', response_type = 'Bruker_bot'):
+                 stimulus_type = 'ternary_dense', response_type = 'model_RGC'):
 
         #population parameters
         self.pop_size = pop_size #individuals in each generation
@@ -40,8 +43,9 @@ class StimulusEvolver():
         self.generation_number = 0  
         self.StimulusGenerator = StimulusGenerator(stimulus_type, stim_size) #maps genome -> stimulus
         
-
-        self.ResponseGenerator = ResponseGenerator(response_type)
+        if threshold is None:
+            threshold = -np.inf
+        self.ResponseGenerator = ResponseGenerator(response_type, noise_level, threshold, stim_size) #maps stimulus -> response
         self.response_type = response_type
     def initializePopulation(self):
         #generate the initial population
@@ -59,12 +63,12 @@ class StimulusEvolver():
         #Get stimuli and responses for each individual in generation
         current_generation_responses = []
         current_generation_fitness = []
-        
-        self.ResponseGenerator.loadResponseData()
+        if self.response_type == 'Bruker_bot':
+            self.ResponseGenerator.loadResponseData()
         for ind_no, genome in enumerate(parent_population):
             _stimulus = self.StimulusGenerator.getStimulusFromGenome(genome) #genome -> stimulus
-            _response = self.ResponseGenerator.getBrukerBotResponse(ind_no) #stimulus -> response
-
+            _response = self.ResponseGenerator.getModelResponse(_stimulus) #stimulus -> response
+                
             current_generation_responses.append(_response)
             _fitness = self.getFitness(_stimulus, _response) # response -> fitness
             current_generation_fitness.append(_fitness)
@@ -147,7 +151,32 @@ class StimulusEvolver():
             vmax = np.max(mean_stim)
             ax.imshow(mean_stim[:,:,tt], cmap=plt.cm.Greys_r, vmin = vmin, vmax = vmax)
             ax.set(xticks=[], yticks=[]) 
-
+            
+        if plot_ind == 0:
+            fig_handle = plt.figure(figsize=(4,4))
+            if self.ResponseGenerator.response_type == 'model_DS':
+                ax = fig_handle.add_subplot(111)
+                ax.imshow(self.ResponseGenerator.xt_center - self.ResponseGenerator.xt_surround)
+                ax.set_axis_off()
+            elif self.ResponseGenerator.response_type == 'model_MotionEnergy':
+                ax1 = fig_handle.add_subplot(211)
+                ax1.imshow(self.ResponseGenerator.xt_1)
+                ax1.set_axis_off()
+                
+                ax2 = fig_handle.add_subplot(212)
+                ax2.imshow(self.ResponseGenerator.xt_2)
+                ax2.set_axis_off()
+            else:
+                for tt in range(self.StimulusGenerator.t_dim):
+                    ax = fig_handle.add_subplot(111)
+                    vmin = np.min(self.ResponseGenerator.RF)
+                    vmax = np.max(self.ResponseGenerator.RF)
+                    ax.imshow(self.ResponseGenerator.RF[0][:,:,tt], cmap=plt.cm.Greys_r, vmin = vmin, vmax = vmax)
+                    ax.set_axis_off()
+                ax = fig_handle.add_subplot(grid[num_rows+2,:])
+                ax.plot(self.ResponseGenerator.temporal_rf)
+                ax.set_axis_off()
+            
 #        fig_handle.savefig('test_results')
             
             
@@ -194,54 +223,118 @@ class StimulusGenerator():
         return row, col
         
 class ResponseGenerator():
-    def __init__(self, response_type):
+    def __init__(self, response_type, noise_level, threshold, stim_size):
         self.response_type = response_type
-
-
-    def loadResponseData(self):
-        date_dir = datetime.now().isoformat()[:-16].replace('-','')
-        cycle_code = ('0000' + str(self.cycle_number))[-5:]
-        bot_suffix = '_Cycle' + cycle_code + '-botData'
-        
-        bot_file_path = os.path.join('E:/Max', date_dir, self.series_name, self.series_name + bot_suffix + '.csv')
+        self.noise_level = noise_level
+        self.threshold = threshold
+        if self.response_type[0:5] == 'model':
+            self.y_dim = stim_size[0] #rows
+            self.x_dim = stim_size[1] #columns
+            self.t_dim = stim_size[2] #depth/time
+            self.getModelRF()
+    
+    def getModelResponse(self, stimulus):
+        if self.response_type[0:5] == 'model':
+            if type(self.RF) is np.ndarray:
+                response_sum = np.sum(stimulus * self.RF)
+            elif type(self.RF) is tuple: #motion energy model
+                resp_1 = np.sum(stimulus * self.RF[0])
+                resp_2 = np.sum(stimulus * self.RF[1])
+                response_sum = (resp_1)**2 + (resp_2)**2
                 
-        print('Waiting for files from PV')
-        print(bot_file_path)
-        while not os.path.exists(bot_file_path):
-            time.sleep(0.5) #wait for the file to appear
-
-        time.sleep(10)
-
-        v_rec_suffix = '_Cycle' + cycle_code + '_VoltageRecording_001'
-        stimulus_timing = imaging_data.getEpochAndFrameTiming(os.path.join('E:/Max', date_dir, self.series_name),
-                                                              self.series_name,
-                                                              v_rec_suffix = v_rec_suffix,
-                                                              plot_trace_flag=False)
+    
+            #add noise to response
+            response = response_sum + self.noise_level * response_sum * np.random.randn()
+    
+            # add a post-integration nonlinearity
+            if self.threshold > -np.inf:
+                if response < self.threshold:
+                    response = 0
+                else:
+                    pass
+            return response
         
-        self.stimulus_start_times = stimulus_timing['stimulus_start_times'].copy() #msec
-        self.stimulus_end_times = stimulus_timing['stimulus_end_times'].copy()
+    def getModelRF(self):
+        if self.response_type == 'model_V1':
+            x, y = np.meshgrid(np.arange(0,self.x_dim), np.arange(0,self.y_dim))
+            xy_tuple = (x,y)
             
-        #read bot data
-        data_frame = pd.read_csv(bot_file_path)
-        self.time_step = data_frame.loc[:]['Timestamp']
-        self.time_step -= self.time_step[0]
-        self.time_step *= 1e3 #sec->msec
-        self.bot_data = data_frame.loc[:]['Region 1']
+            on_lobe = Gauss_2D(xy_tuple, 2, 5, 4, 2, 1, np.deg2rad(45))
+            off_lobe_1 = Gauss_2D(xy_tuple, 1, 4, 3, 2, 1, np.deg2rad(45))
+            off_lobe_2 = Gauss_2D(xy_tuple, 1, 5, 6, 2, 1, np.deg2rad(45))
 
-    def getBrukerBotResponse(self, epoch_number):
-        b_inds_1 = np.where(self.time_step > (self.stimulus_start_times[epoch_number] - self.pre_time))[0]
-        b_inds_2 = np.where(self.time_step <=self.stimulus_start_times[epoch_number])[0]
-        baseline_inds = np.intersect1d(b_inds_1,b_inds_2)
-        baseline = np.mean(self.bot_data[baseline_inds])
-        
-        p_inds_1 = np.where(self.time_step > self.stimulus_start_times[epoch_number])
-        p_inds_2 = np.where(self.time_step <= self.stimulus_end_times[epoch_number])
-        pull_inds = np.intersect1d(p_inds_1,p_inds_2)
+            self.spatial_rf = on_lobe - off_lobe_1 - off_lobe_2
+            self.temporal_rf = getTemporalFilter(self.t_dim)
                 
-        response = np.max((self.bot_data[pull_inds] - baseline) / baseline) #dF/F
+            self.RF = np.swapaxes(np.swapaxes(np.outer(self.temporal_rf,self.spatial_rf).reshape((self.t_dim,self.y_dim,self.x_dim)),0,2),0,1)
+
+        elif self.response_type == 'model_RGC':
+            x, y = np.meshgrid(np.arange(0,self.x_dim), np.arange(0,self.y_dim))
+            xy_tuple = (x,y)
         
-        return response
-        
-  
+            center = Gauss_2D(xy_tuple, 2, 4, 4, 1.5, 1.5, 0)
+            surround = Gauss_2D(xy_tuple, 1, 4, 4, 2.5, 2.5, 0)
+
+            self.spatial_rf = center - surround
+            self.temporal_rf = getTemporalFilter(self.t_dim)
+                
+            self.RF = np.swapaxes(np.swapaxes(np.outer(self.temporal_rf,self.spatial_rf).reshape((self.t_dim,self.y_dim,self.x_dim)),0,2),0,1)
+
+        elif self.response_type == 'model_DS':
+            #spacetime tilted RF:
+            x, t = np.meshgrid(np.arange(0,self.x_dim), np.arange(0,self.t_dim))
+            xt_tuple = (x,t)
+            
+            self.xt_center = Gauss_2D(xt_tuple, 1, 4, 3, 0.5, 6, np.deg2rad(60))
+            self.y_center = Gauss_1D(np.arange(self.y_dim), 2, 4, 0.5)
+            self.RF_center = np.swapaxes(np.outer(self.y_center,self.xt_center).reshape((self.y_dim,self.t_dim,self.x_dim)),1,2)
+            
+            self.xt_surround = Gauss_2D(xt_tuple, 0.5, 4, 3, 1.0, 12, np.deg2rad(60))
+            self.y_surround = Gauss_1D(np.arange(self.y_dim), 2, 4, 0.5)
+            self.RF_surround = np.swapaxes(np.outer(self.y_surround,self.xt_surround).reshape((self.y_dim,self.t_dim,self.x_dim)),1,2)
+            
+            self.RF = self.RF_center - self.RF_surround
+            
+        elif self.response_type == 'model_MotionEnergy':
+            self.xt_1 = skt.resize(np.real(gabor_kernel(0.1, theta = -60, sigma_x=3, sigma_y = 12, n_stds = 2.5, offset = 0)), (self.x_dim, self.t_dim))
+            self.xt_2 = skt.resize(np.real(gabor_kernel(0.1, theta = -60, sigma_x=3, sigma_y = 12, n_stds = 2.5, offset = np.pi/4)), (self.x_dim, self.t_dim))
+            self.y_filter = Gauss_1D(np.arange(self.y_dim), 2, 4, 0.5)
+            
+            self.filter_1 = np.swapaxes(np.outer(self.y_filter,self.xt_1).reshape((self.y_dim,self.t_dim,self.x_dim)),1,2)
+            self.filter_2 = np.swapaxes(np.outer(self.y_filter,self.xt_2).reshape((self.y_dim,self.t_dim,self.x_dim)),1,2)
+            
+            self.RF = (self.filter_1, self.filter_2)
+            
+
+        else:
+            self.RF = 0
+            
+        # normalize RF
+        norm_factor = np.sum(np.abs(self.RF)) #maximum amount the RF can be activated
+        self.RF /= norm_factor
+
+def getTemporalFilter(t_dim):
+    on_time = int(t_dim * 0.5)
+    tFilt = np.ones(shape = t_dim)
+#    tFilt[on_time-3:on_time] = -1
+#    tFilt[on_time+1:on_time+4] = 1
+
+    return tFilt      
+
 def softmax(vec):
     return np.exp(vec) / np.sum(np.exp(vec))
+
+def Gauss_1D(xx, amplitude, x0, sigma):
+    return amplitude * np.exp(-(xx-x0)**2/(2*sigma**2))
+
+def Gauss_2D(xy_tuple, amplitude, xo, yo, sigma_x, sigma_y, theta):
+        # https://en.wikipedia.org/wiki/Gaussian_function
+        # https://stackoverflow.com/questions/21566379/fitting-a-2d-gaussian-function-using-scipy-optimize-curve-fit-valueerror-and-m
+        x, y = xy_tuple
+        xo = float(xo)
+        yo = float(yo)    
+        a = (np.cos(theta)**2)/(2*sigma_x**2) + (np.sin(theta)**2)/(2*sigma_y**2)
+        b = -(np.sin(2*theta))/(4*sigma_x**2) + (np.sin(2*theta))/(4*sigma_y**2)
+        c = (np.sin(theta)**2)/(2*sigma_x**2) + (np.cos(theta)**2)/(2*sigma_y**2)
+        resp = amplitude*np.exp( - (a*((x-xo)**2) + 2*b*(x-xo)*(y-yo) + c*((y-yo)**2)))
+        return resp
